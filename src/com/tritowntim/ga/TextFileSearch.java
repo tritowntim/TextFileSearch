a5b215ce7b21cae07f10740b66a5f0c9936cb8b2
Tim Dussinger <tritowntim@gmail.com>
2012-10-30 08:10:24 -0400

    Validate that file exists at provided path

--- src/com/tritowntim/ga/TextFileSearch.java
+++ src/com/tritowntim/ga/TextFileSearch.java
@@ -1,5 +1,7 @@
 package com.tritowntim.ga;
 
+import java.io.File;
+
 /**
  *
  * @author Tim Dussinger
@@ -10,17 +12,24 @@ public class TextFileSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         validateInput(args);
     }
 
     private static void validateInput(String[] args) {
-        System.out.println(args.length);
         if (args.length != 2) {
             throw new RuntimeException("Program failed: invalid input. "
                     + "Please provide the complete file path "
                     + "and search string "
                     + "as arguments to this program.");
+        } else if (!fileExists(args[0])) {
+            throw new RuntimeException("Program failed: file not found. "
+                    + "There was no file found at the provided path of '"
+                    + args[0]
+                    + "'. Please try again.");
         }
+    }
+
+    static boolean fileExists(String path) {
+        return (new File(path)).exists();
     }
 }
--- test/com/tritowntim/ga/TextFileSearchTest.java
+++ test/com/tritowntim/ga/TextFileSearchTest.java
@@ -2,35 +2,45 @@ package com.tritowntim.ga;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author Tim Dussinger
  */
 public class TextFileSearchTest {
-    
-    
+
+    private String mainSourceFile;
+    private String invalidSourceFile;
+    private final String searchForThisText = "search-for-this-text";
+        
+    @Before
+    public void setup() {
+        mainSourceFile = System.getProperty("user.dir") + "/src/com/tritowntim/ga/TextFileSearch.java";
+        invalidSourceFile = System.getProperty("user.dir") + "/src/com/tritowntim/ga/TextFileSearch-does-not-exist.java";
+    }
+
     @Test()
     public void twoArgs() throws Exception {
         String[] arguments = new String[2];
-        arguments[0] = "a";
+        arguments[0] = mainSourceFile;
         arguments[1] = "b";
         TextFileSearch.main(arguments);
     }
 
-    @Test(expected=RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void zeroArgs() throws Exception {
         TextFileSearch.main(null);
     }
 
-    @Test(expected=RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void oneArg() throws Exception {
         String[] arguments = new String[1];
         arguments[0] = "a";
         TextFileSearch.main(arguments);
     }
 
-    @Test(expected=RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void threeArg() throws Exception {
         String[] arguments = new String[3];
         arguments[0] = "a";
@@ -38,6 +48,30 @@ public class TextFileSearchTest {
         arguments[2] = "c";
         TextFileSearch.main(arguments);
     }
-    
-    
+
+    @Test
+    public void fileExists() throws Exception {
+        assertTrue(TextFileSearch.fileExists(mainSourceFile));
+    }
+
+    @Test
+    public void fileDoesNotExist() throws Exception {
+        assertFalse(TextFileSearch.fileExists(invalidSourceFile));
+    }
+
+    @Test
+    public void validFile() throws Exception {
+        String[] arguments = new String[2];
+        arguments[0] = mainSourceFile;
+        arguments[1] = searchForThisText;
+        TextFileSearch.main(arguments);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void invalidFile() throws Exception {
+        String[] arguments = new String[2];
+        arguments[0] = invalidSourceFile;
+        arguments[1] = searchForThisText;
+        TextFileSearch.main(arguments);
+    }
 }
